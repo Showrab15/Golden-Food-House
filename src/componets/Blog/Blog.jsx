2eda14adc11e7620555b1034a52deb68033f3ab2
Showrab15 <supto50showrab@gmail.com>
2023-05-04 22:36:23 +0600

    recipe card added for eacj chef

--- src/componets/Blog/Blog.jsx
+++ src/componets/Blog/Blog.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-// import ReactToPdf from '../ReactToPdf/ReactToPdf';
-// import { PDFDownloadLink } from '@react-pdf/renderer';
+
 import ReactDOM from "react-dom";
  import  Pdf from  "react-to-pdf";
 
@@ -11,29 +10,29 @@ const Blog = () => {
   return (
    <div>
     <div className='text-center mt-10'>
-                <Pdf targetRef={ref} filename="code-example.pdf">
-        {({ toPdf }) => <button className='btn btn-primary' onClick={toPdf}>Generate Blog Pdf</button>}
+                <Pdf targetRef={ref} filename="blog-question-assignment10-showrab">
+        {({ toPdf }) => <button className='btn btn-secondary' onClick={toPdf}>Download  Pdf</button>}
       </Pdf>
       </div>
 
      <div>
       <section ref={ref} className="mt-8 mb-8 w-[50%] mx-auto">
-        <h1 className="text-3xl font-bold text-center  ">Few Question Answer</h1>
+        <h1 className="text-3xl font-bold text-center">Blog Question Answer</h1>
         <hr style={{ border:" 1px solid  #FF900E"}}  className="w-48 mx-auto mt-2 " />
 <hr style={{ border:" 1px solid  #FF900E"}}  className="w-32 mx-auto mt-2 mb-8 " />
      <h1 className='text-2xl font-bold '>  1. Tell us the differences between uncontrolled and controlled components</h1>
      <p className="font-semibold">Ans: In React, controlled components refer to components that have their state and behavior controlled by the parent component. These components rely on props passed down from the parent component to update their state and behavior. Uncontrolled components refer to components that manage their own state internally</p>
-     <h1 className='text-2xl font-bold '>  2. How to validate React props using PropTypes ?
+     <h1 className='text-2xl font-bold'>  2. How to validate React props using PropTypes ?
 </h1>
      <p className="font-semibold"> Ans:
  
 
 Import the PropTypes library in your component using import PropTypes from 'prop-types'; Then Define the expected data types for your props using the propTypes object inside your component. Specify the data type for each prop using PropTypes, such as PropTypes.string, PropTypes.number, PropTypes.bool, etc. Must be Assign the propTypes object to your component using the MyComponent.propTypes syntax.</p>
 
-     <h1 className='text-2xl font-bold '>  3. Tell us the difference between nodejs and express js?</h1>
+     <h1 className='text-2xl font-bold'>  3. Tell us the difference between nodejs and express js?</h1>
      <p className="font-semibold">Ans: js web application framework that provides a robust set of features for web and mobile applications. In other words, NodeJS is the package, which provides the JavaScript run-time environment, whereas Express is a framework that sits on top of NodeJS and helps us to handle requests and responses.</p>
 
-     <h1 className='text-2xl font-bold '>   4. What is a custom hook, and why will you create a custom hook?
+     <h1 className='text-2xl font-bold'>   4. What is a custom hook, and why will you create a custom hook?
 </h1>
      <p className="font-semibold">Ans: Custom hooks allow you to extract and reuse stateful logic from components, reducing duplication and keeping your code more modular and easier to maintain.Custom hooks are created by defining a function that uses the built-in React hooks like useState, useEffect, useContext, etc., and returns some state or behavior that can be used by components.</p>
    
@@ -48,8 +47,4 @@ Import the PropTypes library in your component using import PropTypes from 'prop
 
 export default Blog;
 
-  // <div className="flex justify-center mx-auto">
-  //     <PDFDownloadLink document={<ReactToPdf/>} fileName='form'>
-  //      {({loading}) => (loading ? <button >Loading Document...</button> : <button className="btn btn-primary ">Download Question Answser  </button> )}
-  //      </PDFDownloadLink>
-  //     </div>
+ 
